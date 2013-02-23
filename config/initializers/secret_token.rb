415c892e8c486ae843a245761199a86799b61496
mariozig <mariozig@gmail.com>
2013-02-22 16:56:21 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skrimply::Application.config.secret_token = '305efd514b3cba815bc8d1c7ad49d837537c2e68abebbf8b2606ebe712388b0bbc2e0b0a641a10f2dc6a234630d86dd6f2ec11b5a017965563ae79d1900834cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Skrimply::Application.config.session_store :cookie_store, key: '_skrimply_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Skrimply::Application.config.session_store :active_record_store
