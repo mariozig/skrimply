3d107fcc261012ebe63eb999fb228b5ba378111a
mariozig <mariozig@gmail.com>
2013-02-24 22:40:18 -0800

    Association Typos
    Corrected a few typos made during the addition of model association stuff.

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,10 +1,10 @@
 class Track < ActiveRecord::Base
   has_many :definitions
-  has_many :artists_tracks
+  has_many :artist_tracks
   has_many :artists, :through => :artists_tracks
-  has_many :genres_tracks
-  has_many :genres, :through => :genres_tracks
-  has_many :releases_tracks
+  has_many :genre_tracks
+  has_many :genres, :through => :genre_tracks
+  has_many :release_tracks
   has_many :releases, :through => :releases_tracks
 
   attr_accessible :duration, :lyrics, :name
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :definitions
 
-  attr_accessible :email, :name
+  attr_accessible :email, :first_name, :last_name
 end
