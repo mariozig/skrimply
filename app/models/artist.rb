330e4e4e2d63a73a98faa5d976c393945ac312f6
mariozig <mariozig@gmail.com>
2013-02-25 10:56:45 -0800

    Added a couple of scopes to Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,6 +6,13 @@ class Artist < ActiveRecord::Base
   has_many :artist_genres
   has_many :genres, :through => :artist_genres
 
-
   attr_accessible :name
+
+  def tracks_owned
+    tracks.where(:artist_tracks => { :artistic_role_id => ArtisticRole.owner.id} )
+  end
+
+  def tracks_featured_on
+    tracks.where(:artist_tracks => { :artistic_role_id => ArtisticRole.featured.id } )
+  end
 end
