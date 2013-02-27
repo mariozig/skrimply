1efd049faad3c67f982e1b5cffb4f4e6f928b932
mariozig <mariozig@gmail.com>
2013-02-27 10:25:46 -0800

    Completed releases
    views/controller/model refactor… all in.

---
+++ app/controllers/releases_controller.rb
@@ -0,0 +1,45 @@
+class ReleasesController < ApplicationController
+
+  def index
+    @releases = Release.all
+  end
+
+  def show
+    @release = Release.find(params[:id])
+  end
+
+  def new
+    @release = Release.new
+  end
+
+  def edit
+    @release = Release.find(params[:id])
+  end
+
+  def create
+    @release = Release.new(params[:release])
+
+    if @release.save
+      redirect_to @release, notice: 'Release created.'
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @release = Release.find(params[:id])
+
+    if @release.update_attributes(params[:release])
+      redirect_to @release, notice: 'Release updated.'
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @release = Release.find(params[:id])
+    @release.destroy
+
+    redirect_to releases_url
+  end
+end
--- app/models/release.rb
+++ app/models/release.rb
@@ -4,10 +4,19 @@ class Release < ActiveRecord::Base
 
   has_many :genre_releases
   has_many :genres, :through => :genre_releases
-  has_many :artist_releases
-  has_many :artists, :through => :artist_releases
   has_many :release_tracks
   has_many :tracks, :through => :release_tracks
+  has_many :owning_artist_releases, :class_name => "ArtistRelease", :conditions => { :artistic_role_id => ArtisticRole.owner }
+  has_many :owning_artists, :through => :owning_artist_releases, :source => :artist
 
   attr_accessible :label_id, :location, :name, :release_date, :release_kind_id
+
+  # Since we typically only have 1 release owner, this is a convenience method
+  def owning_artist
+    owning_artists.first
+  end
+
+  def label_name
+    label.nil? ? "unknown" : label.name
+  end
 end
