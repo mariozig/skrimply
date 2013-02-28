a78ad608a1369b01d70e530ed9a42779a176f36a
mariozig <mariozig@gmail.com>
2013-02-27 20:44:06 -0800

    Use devise in modals.  This also required that I create some helper methods in the application helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,22 @@
 module ApplicationHelper
-  # Added by devise
-  # before_filter :authenticate_user!
+  # Provided by devise
+  #
   # user_signed_in?
   # current_user
   # user_session
+
+  # Source:
+  # https://github.com/plataformatec/devise/wiki/How-To:-Display-a-custom-sign_in-form-anywhere-in-your-app
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
