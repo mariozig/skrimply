6fcced2b0b5f47e2e3830b53fbb67bca9e81a469
mariozig <mariozig@gmail.com>
2013-03-07 10:10:05 -0800

    "highlightable" ranges and their associated definitions can now be determined and are stuffed into a massive object.  This is prime real estate in Refactoropolis.

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -18,64 +18,10 @@ $(function() {
                            "$1$highlight$lyrics";
 
     highlighter.deserialize(serializedHighlights);
+    var highlightableRanges = getHighlightableRanges(definitions);
 
-    var allDefinedCharacters = function(){
-      return _.flatten(_.map(definitions, function(definition){
-        return _.range(definition.range_start, definition.range_end);
-      }));
-    }();
+    console.log(highlightableRanges);
 
-    var characterIndexToOverlapCount = function(){
-      var characterOverlaps = {};
-      _.each(allDefinedCharacters, function(character){
-        if(_.has(characterOverlaps, character)){
-          characterOverlaps[character] = characterOverlaps[character] + 1;
-        } else {
-          characterOverlaps[character] = 1;
-        }
-      });
-
-      return characterOverlaps;
-    }()
-
-    var overlapCountToIndexes = {};
-    var rangezilla = {};
-    var values = _.uniq(_.values(characterIndexToOverlapCount));
-
-    // builds the keys
-    _.each(values, function(value){
-      overlapCountToIndexes[value] = [];
-      rangezilla[value] = [];
-    });
-
-    // Populates with indexes
-    _.each(_.keys(characterIndexToOverlapCount), function(key){
-      overlapCountToIndexes[characterIndexToOverlapCount[key]].push(key);
-    });
-
-    // Builds out rangezilla.
-    var sequences = [];
-    _.each(_.keys(overlapCountToIndexes), function(key){
-      var values = overlapCountToIndexes[key].sort(sortByNumber);
-      var temp = [];
-      for (var i = 0; i < values.length; i++) {
-        var thisPlusOne = parseInt(values[i]) + 1;
-        var nextItem = parseInt(values[i+1]);
-        temp.push(values[i]);
-        if(thisPlusOne !== nextItem){
-          sequences.push(temp);
-          temp = [];
-        }
-      };
-
-      rangezilla[key].push(sequences);
-      sequences = [];
-    });
-
-    console.log("overlapCountToIndexes");
-    console.log(overlapCountToIndexes);
-    console.log("rangezilla");
-    console.log(rangezilla);
   })
   // .success(function() { alert("second success"); })
   // .error(function() { alert("error"); })
@@ -109,6 +55,78 @@ $(function() {
     $("#definitionModal").modal('toggle');
   });
 
+  function getHighlightableRanges(definitions){
+    var allDefinedCharacters = function(){
+      return _.flatten(_.map(definitions, function(definition){
+        return _.range(definition.range_start, definition.range_end+1);
+      }));
+    }();
+
+    var characterIndexToOverlapCount = function(){
+      var characterOverlaps = {};
+      _.each(allDefinedCharacters, function(character){
+        if(_.has(characterOverlaps, character)){
+          characterOverlaps[character] = characterOverlaps[character] + 1;
+        } else {
+          characterOverlaps[character] = 1;
+        }
+      });
+
+      return characterOverlaps;
+    }()
+
+    var overlapCountToIndexes = {};
+    var highlightableRanges = {};
+    var values = _.uniq(_.values(characterIndexToOverlapCount));
+
+    // builds the keys
+    _.each(values, function(value){
+      overlapCountToIndexes[value] = [];
+      highlightableRanges[value] = [];
+    });
+
+    // Populates with indexes
+    _.each(_.keys(characterIndexToOverlapCount), function(key){
+      overlapCountToIndexes[characterIndexToOverlapCount[key]].push(key);
+    });
+
+    // Builds out highlightableRanges.
+    _.each(_.keys(overlapCountToIndexes), function(key){
+      // Sets all of the range starts/stops
+      var values = overlapCountToIndexes[key].sort(sortByNumber);
+      var temp = [];
+      for (var i = 0; i < values.length; i++) {
+        var thisPlusOne = parseInt(values[i]) + 1;
+        var nextItem = parseInt(values[i+1]);
+        temp.push(values[i]);
+        if(thisPlusOne !== nextItem){
+          highlightableRanges[key].push({
+            "range": {
+              "start": temp[0],
+              "end": temp[temp.length-1]
+            }
+          });
+          temp = [];
+        }
+      };
+    });
+
+    // populates the associated definitions
+    _.each(_.keys(highlightableRanges), function(key){
+      _.each(highlightableRanges[key], function(sequence) {
+        _.each(definitions, function(definition) {
+          if(rangesIntersect({start: definition.range_start, end: definition.range_end}, sequence.range)){
+            if(sequence["definitions"] === undefined){
+              sequence["definitions"] = [];
+            }
+            sequence["definitions"].push(definition);
+          }
+        });
+      });
+    });
+
+    return highlightableRanges;
+  }
   // Returns true if a selection is considered valid
   // A selection is valid under the following conditions:
   // - It contains DOM nodes
@@ -128,4 +146,12 @@ $(function() {
     return(a-b)
   }
 
+  function rangesIntersect(range1, range2){
+    if(range1.start >= range2.start && range1.start <= range2.end)
+      return true;
+    if(range1.end >= range2.start && range1.start <= range2.end)
+      return true;
+
+    return false;
+  }
 });
