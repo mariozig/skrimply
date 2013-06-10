996920fc731db0b79940424b757e25fceef47cef
mariozig <mariozig@gmail.com>
2013-06-10 13:59:25 -0700

    Rewrite model associations to use lambda conditions instead of :conditions, which is deprecated in rails 4.

--- app/models/track.rb
+++ app/models/track.rb
@@ -17,11 +17,9 @@ class Track < ActiveRecord::Base
   has_many :definitions
   has_many :artist_tracks
   has_many :artists, :through => :artist_tracks
-  has_many :owning_artist_tracks, :class_name => "ArtistTrack",
-                                  :conditions => { :artistic_role_id => ArtisticRole.owner }
+  has_many :owning_artist_tracks, -> { where artistic_role_id: ArtisticRole.owner }, :class_name => "ArtistTrack"
   has_many :owning_artists, :through => :owning_artist_tracks, :source => :artist
-  has_many :featured_artist_tracks, :class_name => "ArtistTrack",
-                                    :conditions => { :artistic_role_id => ArtisticRole.featured }
+  has_many :featured_artist_tracks, -> { where artistic_role_id: ArtisticRole.featured }, :class_name => "ArtistTrack"
   has_many :featuring_artists, :through => :featured_artist_tracks, :source => :artist
   has_many :genre_tracks
   has_many :genres, :through => :genre_tracks
